5ac33aad3ebf9d9210bdc6b5aec45814f009a547
Boonyarit Phothong <unlove_unhurt@hotmail.com>
2012-06-19 16:03:56 +0700

    เพิ่ม Programming is fun

--- src/nu/ac/student/u54347613/compro01/HelloWorld.java
+++ src/nu/ac/student/u54347613/compro01/HelloWorld.java
@@ -1,13 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nu.ac.student.u54347613.compro01;
 
-/**
- *
- * @author Owner
- */
 public class HelloWorld {
+    
+    public static void main(String [] args){
+        
+        System.out.println("Hello,World");
+        System.out.println("Programming is fun.");
+    }
     
 }
